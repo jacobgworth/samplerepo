7ffd232d0b52d28d757e56d999c85663eb878f0c
David Dewhirst <dsdewhir@gmail.com>
2012-03-10 13:03:15 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mohhaiti::Application.config.secret_token = 'aa781ab16642ccf45f2b56acc68924439bb60eff5a97ca5e14d318d3807a53e720a1de49d4dbf76dfa7e4bfaeda02b198aba29afb31b9f9194f2a2df62de1c7e'
