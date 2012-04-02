c143a1d48b7cd7eac5161fe9f7dde24bc69cffff
David Dewhirst <dsdewhir@gmail.com>
2012-04-02 12:44:20 -0400

    .

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,11 @@
 class Project < ActiveRecord::Base
+  acts_as_gmappable :process_geocoding => false
+  
+  def gmaps4rails_address
+    "#{self.latitude}, #{self.longitude}"
+  end
+  
+  def gmaps4rails_title
+    "#{self.projectname}"
+  end
 end
