9ad5dd61bc43f891295790083316231dce5f60be
David Dewhirst <dsdewhir@gmail.com>
2012-05-21 17:03:21 -0400

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,8 @@ Mohhaiti::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # Don't fallback to assets pipeline if a precompiled asset is missed -- this was false, dsd changed it
+  config.assets.compile = true
   
   config.assets.precompile += %w( *.js *.css )  
 
