bd23a932e0b50c09f158469087a674011032edcc
David Dewhirst <dsdewhir@gmail.com>
2012-05-21 09:53:22 -0400

    .

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,8 @@ module Mohhaiti
     # Enable the asset pipeline
     config.assets.enabled = true
     
+    # Precompile gmaps4rails
+    config.assets.precompile += %w( *.js *.css )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
