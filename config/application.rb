2f3709469d8557b180bfac131c0888726e1b180a
David Dewhirst <dsdewhir@gmail.com>
2012-05-21 15:15:54 -0400

    .

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module Mohhaiti
     config.assets.enabled = true
     
     # Precompile gmaps4rails
-    config.assets.precompile += %w( *.js *.css )
+    # config.assets.precompile += %w( *.js *.css )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
