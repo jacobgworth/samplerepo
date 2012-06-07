73d81a77f5bd9a338f9217fa76c20fece177b905
David Dewhirst <dsdewhir@gmail.com>
2012-06-07 10:26:04 -0400

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Mohhaiti::Application.routes.draw do
 
+  root :to => 'home#index'
 
   resources :contents
 
@@ -85,7 +86,7 @@ Mohhaiti::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
