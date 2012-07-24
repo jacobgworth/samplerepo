26a6db16536b5fd2c36e5e69e0fdef250984b5b7
David Dewhirst <dsdewhir@gmail.com>
2012-07-24 11:47:02 -0400

    Adding Asset model to allow multiple photos in Experience

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120719130830) do
+ActiveRecord::Schema.define(:version => 20120724153839) do
+
+  create_table "assets", :force => true do |t|
+    t.string   "asset_file_name"
+    t.string   "asset_content_type"
+    t.integer  "asset_file_size"
+    t.datetime "asset_updated_at"
+    t.integer  "experience_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "communities", :force => true do |t|
     t.string   "communityname"
---
+++ test/unit/sset_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SsetTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
