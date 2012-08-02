4ac7c74225e7cd6efce0292bc899d6f065724d80
Robert Deluca <robertdeluca19@Roberts-MacBook-Pro.local>
2012-08-02 11:54:36 -0400

    Started Church advancement page. Still needs the bottom half done. Not sure if there are styles already done in CSS for the bottom borders or not. Comitting and pushing now to work R&R.

--- app/controllers/advancement_controller.rb
+++ app/controllers/advancement_controller.rb
@@ -1,5 +1,7 @@
 class AdvancementController < ApplicationController
   def index
+      respond_to do |format|
+      format.html {render :layout=>"homeLayout"}# haiti_one.html.erb
+    end
   end
-
 end
