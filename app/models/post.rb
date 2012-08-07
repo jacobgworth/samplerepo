be740278f9b17224080859c3bf28a8f9faa25f47
David Dewhirst <dsdewhir@gmail.com>
2012-08-07 11:56:19 -0400

    Adding multiple image attachments to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,12 @@
 class Post < ActiveRecord::Base
 
+  has_many :assets
   belongs_to :user
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :projects
 
-  attr_accessible :title,:user_id,:body,:imagelink,:category_ids,:project_ids
+  attr_accessible :title,:user_id,:body,:imagelink,:category_ids,:project_ids,:assets_attributes
+  
+  accepts_nested_attributes_for :assets, :allow_destroy => true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120807154451) do
+ActiveRecord::Schema.define(:version => 20120807155238) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20120807154451) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "project_id"
+    t.integer  "post_id"
   end
 
   create_table "categories", :force => true do |t|
