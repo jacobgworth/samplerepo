d1423c48b8b980823daf4653b7018019837d18c2
David Dewhirst <dsdewhir@gmail.com>
2012-08-10 12:34:50 -0400

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,28 +19,46 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
+    if is_admin_user?
+      @user = User.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
-    end
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @user }
+      end
+     else
+       respond_to do |format|
+        format.html { redirect_to "/" }
+      end
+     end
   end
 
   # GET /users/new
   # GET /users/new.json
   def new
-    @user = User.new
+    if is_admin_user?
+      @user = User.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
+      respond_to do |format|
+        format.html # new.html.erb
+        format.json { render json: @user }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to "/" }
+      end
     end
   end
 
   # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
+    if is_admin_user?
+      @user = User.find(params[:id])
+    else
+      respond_to do |format|
+        format.html { redirect_to "/" }
+      end
+    end
   end
 
   # POST /users
@@ -78,12 +96,18 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user = User.find(params[:id])
-    @user.destroy
+    if is_admin_user?
+      @user = User.find(params[:id])
+      @user.destroy
 
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :ok }
+      respond_to do |format|
+        format.html { redirect_to users_url }
+        format.json { head :ok }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to "/" }
+      end
     end
   end
 end
