2071b76447ace0ea567162a0237f186901d1b745
David Dewhirst <dsdewhir@gmail.com>
2012-08-12 12:44:11 -0400

    .

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -4,5 +4,7 @@ class Partner < ActiveRecord::Base
   
   has_and_belongs_to_many :projects
   
+  attr_accessible :partnername,:community_ids
+  
 
 end
--- app/models/update.rb
+++ app/models/update.rb
@@ -5,5 +5,8 @@ class Update < ActiveRecord::Base
   has_and_belongs_to_many :projects
   has_and_belongs_to_many :tags
   belongs_to :user
+  
+
+
 
 end
