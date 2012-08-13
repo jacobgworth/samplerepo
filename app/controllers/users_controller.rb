b5ef35e3588c731246b541824771257a02f900a3
David Dewhirst <dsdewhir@gmail.com>
2012-08-13 15:39:07 -0400

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    if true #is_admin_user?
+    if is_admin_user?
       @users = User.all
 
       respond_to do |format|
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    if true #is_admin_user?
+    if is_admin_user?
       @user = User.find(params[:id])
 
       respond_to do |format|
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
   # GET /users/new
   # GET /users/new.json
   def new
-    if true #is_admin_user?
+    if is_admin_user?
       @user = User.new
 
       respond_to do |format|
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    if true #is_admin_user?
+    if is_admin_user?
       @user = User.find(params[:id])
     else
       respond_to do |format|
