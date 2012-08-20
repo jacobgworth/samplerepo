cd8e6b934b73629e0654b038a21dabb458618e40
David Dewhirst <dsdewhir@gmail.com>
2012-08-20 16:18:28 -0400

    Event model created

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   has_and_belongs_to_many :communities
   has_and_belongs_to_many :projects
 
-  attr_accessible :title,:user_id,:body,:imagelink,:category_ids,:project_ids,:assets_attributes,:author,:postdate
+  attr_accessible :title,:user_id,:body,:imagelink,:category_ids,:project_ids,:community_ids,:assets_attributes,:author,:postdate
   
   accepts_nested_attributes_for :assets, :allow_destroy => true
 
