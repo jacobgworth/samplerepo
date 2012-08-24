7567ec420d7c955ec85c99977784fcd95447002c
David Dewhirst <dsdewhir@gmail.com>
2012-08-24 12:47:28 -0400

    Church Advancement Page done

--- app/controllers/advancement_controller.rb
+++ app/controllers/advancement_controller.rb
@@ -1,6 +1,13 @@
 class AdvancementController < ApplicationController
   def index
-    @updates=Update.all
+    
+    @category = Category.find_by_categoryname("Church Advancement")
+    @projects = Project.joins(:categories).where("category_id=" + @category.id.to_s).last(6).reverse
+    @posts = Post.joins(:categories).where("category_id=" + @category.id.to_s).last(3).reverse
+    @updates = Update.joins(:categories).where("category_id=" + @category.id.to_s).last(3).reverse
+    @villages = Community.joins(:projects=>:categories).where("category_id=?",@category.id.to_s)
+    
+    
       respond_to do |format|
       format.html {render :layout=>"homeLayout"}# haiti_one.html.erb
     end
