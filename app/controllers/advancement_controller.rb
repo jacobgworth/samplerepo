aed2781d6bb1d08c6483b62faafa7e955b5f8804
David Dewhirst <dsdewhir@gmail.com>
2012-09-04 12:26:32 -0400

    .

--- app/controllers/advancement_controller.rb
+++ app/controllers/advancement_controller.rb
@@ -1,14 +1,14 @@
 class AdvancementController < ApplicationController
   def index
     
-   # @category = Category.find_by_categoryname("Church Advancement")
-   #@projects = Project.joins(:categories).where("category_id=" + @category.id.to_s).last(6).reverse
-    #@posts = Post.joins(:categories).where("category_id=" + @category.id.to_s).last(3).reverse
-    #@updates = Update.joins(:categories).where("category_id=" + @category.id.to_s).last(3).reverse
-    #@villages = Community.joins(:projects=>:categories).where("category_id=?",@category.id.to_s)
+   @category = Category.find_by_categoryname("Church Advancement")
+   @projects = Project.joins(:categories).where("category_id=" + @category.id.to_s).last(6).reverse
+   @posts = Post.joins(:categories).where("category_id=" + @category.id.to_s).last(3).reverse
+   @updates = Update.joins(:categories).where("category_id=" + @category.id.to_s).last(3).reverse
+   @villages = Community.joins(:projects=>:categories).where("category_id=?",@category.id.to_s)
     
     
-      respond_to do |format|
+    respond_to do |format|
       format.html {render :layout=>"homeLayout"}# haiti_one.html.erb
     end
   end
