c8fbd619e11a528e1f14b47b0d61470cdab4a964
David Dewhirst <dsdewhir@gmail.com>
2012-09-11 16:52:14 -0400

    .

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -15,6 +15,7 @@ class CommunitiesController < ApplicationController
   # GET /communities/1
   # GET /communities/1.json
   def show
+    @user = current_user
     @community = Community.find(params[:id])
 
     respond_to do |format|
@@ -26,17 +27,29 @@ class CommunitiesController < ApplicationController
   # GET /communities/new
   # GET /communities/new.json
   def new
-    @community = Community.new
+    if is_admin_user?
+      @community = Community.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @community }
-    end
+      respond_to do |format|
+        format.html # new.html.erb
+        format.json { render json: @community }
+      end
+     else
+      respond_to do |format|
+        format.html { redirect_to "/communities/" }
+      end
+     end
   end
 
   # GET /communities/1/edit
   def edit
-    @community = Community.find(params[:id])
+    if is_admin_user?
+      @community = Community.find(params[:id])
+    else
+      respond_to do |format|
+        format.html { redirect_to "/communities/" }
+      end
+    end
   end
 
   # POST /communities
