c1eb1e1a1c60504039a5f4986fac6fd60fcfc4b6
David Dewhirst <dsdewhir@gmail.com>
2012-09-23 14:15:00 -0400

    Adding posts and updates to Stay Connected

--- app/controllers/connected_controller.rb
+++ app/controllers/connected_controller.rb
@@ -2,6 +2,8 @@ class ConnectedController < ApplicationController
   def index
     
     @posts = Post. last(5).reverse
+    @updates = Update.last(3).reverse
+    @videos = Video.last(3).reverse
     @user = current_user
     respond_to do |format|
       format.html {render :layout=>"homeLayout"}# connected.html.erb
