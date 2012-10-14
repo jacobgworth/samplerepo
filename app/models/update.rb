eefa42a1014cd615bd4ac0c6c7086fa51854d656
David Dewhirst <dsdewhir@gmail.com>
2012-10-14 16:31:33 -0400

    Adding photos to updates

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,5 +1,6 @@
 class Update < ActiveRecord::Base
 
+  has_many :assets
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :communities
   has_and_belongs_to_many :projects
@@ -7,6 +8,7 @@ class Update < ActiveRecord::Base
   belongs_to :user
   
 
+  accepts_nested_attributes_for :assets, :allow_destroy => true
 
 
 end
