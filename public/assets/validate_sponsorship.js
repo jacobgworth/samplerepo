7da0f385877b9279bd467775dcc53eb98933643a
Jesse Briggs <jesse@threetwelvecreative.com>
2012-10-24 16:52:27 -0400

    Add basic form validation for sponsorship_contact

---
+++ public/assets/validate_sponsorship.js
@@ -0,0 +1,47 @@
+function validate() {
+	//verify fields are not empty	
+	var missing = ""
+	if (document.getElementById('txtname').value == "")
+	{
+		missing += "Please enter your name.";
+	}
+	if (document.getElementById('txtphone').value == "")
+	{
+		missing += "\nPlease enter your phone number.";
+	}
+	if (document.getElementById('txtemail').value == "")
+	{
+		missing += "\nPlease enter your email.";
+	} else {
+	    if (validateEmail(document.getElementById('txtemail').value) == false) {
+	    	missing += "\nPlease enter a valid email address.";
+	    }
+	} 
+	if (document.getElementById('txtaddress').value == "")
+	{
+		missing += "\nPlease enter your address.";
+	}
+	if (document.getElementById('txtcity').value == "")
+	{
+		missing += "\nPlease enter your city.";
+	}
+	if (document.getElementById('txtstate').value == "")
+	{
+		missing += "\nPlease enter your state.";
+	}
+	if (document.getElementById('txtzip').value == "")
+	{
+		missing += "\nPlease enter your zip.";
+	}
+		
+	
+	if (missing == "") { return true; }
+	else { alert(missing); return false; }
+}
+
+function validateEmail(elementValue){
+   //test elementValue against regex for email
+   var reg = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+   var result = reg.test(elementValue);
+   return result;
+} 
