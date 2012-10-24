7699eeaf3550e9cc93c952833478db95974dc828
Jesse Briggs <jesse@threetwelvecreative.com>
2012-10-24 15:43:35 -0400

    Add email validation functionality to validate_volunteer.js

--- public/assets/validate_volunteer.js
+++ public/assets/validate_volunteer.js
@@ -12,12 +12,24 @@ function validate() {
 	if (document.getElementById('txtemail').value == "")
 	{
 		missing += "\nPlease enter your email.";
-	}
+	} else {
+	    if (validateEmail(document.getElementById('txtemail').value) == false) {
+	    	missing += "\nPlease enter a valid email address.";
+	    }
+	} 
 	if (document.getElementById('txtmessage').value == "")
 	{
 		missing += "\nPlease enter a message for us.";
 	}
+		
 	
 	if (missing == "") { return true; }
 	else { alert(missing); return false; }
 }
+
+function validateEmail(elementValue){
+   //test elementValue against regex for email
+   var reg = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+   var result = reg.test(elementValue);
+   return result;
+} 
