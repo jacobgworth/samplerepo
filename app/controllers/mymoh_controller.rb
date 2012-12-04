92d9c706bd99b0681ee40c64705d989a9539d2eb
Jesse Briggs <jesse@threetwelvecreative.com>
2012-12-04 18:14:19 -0500

    Display some account info.

--- app/controllers/mymoh_controller.rb
+++ app/controllers/mymoh_controller.rb
@@ -2,8 +2,21 @@ class MymohController < ApplicationController
   include Databasedotcom::Rails::Controller
   
   def account
+    @account = Contact.find_by_name("Lindsey Rubino")
     respond_to do |format|
       format.html {render :layout=>"homeLayout"} 
+    end
+  end
+  
+  def new
+    if request.method == "POST"
+       
+    else
+       @user = User.new 
+       
+       respond_to do |format|
+         format.html #new.html.erb
+       end
     end
   end
 end
