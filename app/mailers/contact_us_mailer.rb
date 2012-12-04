3a113f9a8bb672572ce2e088e67306faa38e9b83
Jesse Briggs <jesse@threetwelvecreative.com>
2012-12-04 08:53:17 -0500

    Change "medical trip" email to dana@mohaiti

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -86,7 +86,7 @@ class ContactUsMailer < ActionMailer::Base
     @medical = @data[:medical]
     @toemail = "missiontrips@mohhaiti.org"
     if (@data[:medical] == "on")
-       @toemail = "medicalmissiontrips@mohhaiti.org" 
+       @toemail = "dana@mohhaiti.org" 
     end
     
     mail(:to => @toemail, :subject => "Take a Mission Trip", :from => @fromaddress)
