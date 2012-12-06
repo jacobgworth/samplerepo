b9c25395b0745122319d46a2a19ef854c0b6d045
Jesse Briggs <jesse@threetwelvecreative.com>
2012-12-06 10:44:44 -0500

    Clean up on mymoh#account

--- app/controllers/mymoh_controller.rb
+++ app/controllers/mymoh_controller.rb
@@ -2,9 +2,17 @@ class MymohController < ApplicationController
   include Databasedotcom::Rails::Controller
   
   def account
-    @account = Contact.find_by_name("Lindsey Rubino")
+    @account = Contact.find_by_Id(current_user.convio_id)
     respond_to do |format|
       format.html {render :layout=>"homeLayout"} 
+    end
+  end
+  
+  def giving
+    #@contact = Contact.find_by_Id(current_user.convio_id)
+    @account = Contact.find_by_Name("Joseph Leake")
+    respond_to do | format |
+      format.html { render :layout => "homeLayout" }
     end
   end
   
