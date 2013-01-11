70dd3f25c49171bd0e7280ecef72df07bd0ef422
Jesse Briggs <jesse@threetwelvecreative.com>
2013-01-11 10:11:03 -0500

    Set proper fund % on /bluetoblock

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,10 +12,10 @@ class ProjectsController < ApplicationController
     @category = @project.categories.first
     @updates = @project.updates.last(3).reverse #Update.last(3).reverse
     @posts = @project.posts.order(:postdate).reverse_order.last(3)
-    @fundpercent = 76
+    @fundpercent = 64
     if !@project.fundsneeded.nil? && @project.fundsneeded > 0
       #@fundpercent = (@project.fundsraised/@project.fundsneeded) * 100.00
-      @fundpercent = 76
+      @fundpercent = 64
     end
     @fundPercentImage = 'percent0.png'
     if @fundpercent >= 10 && @fundpercent < 20
