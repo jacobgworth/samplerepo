1d3bbf41c006bb0bf4edb0cadeb2ccdfe390e2af
Jesse Briggs <jesse@threetwelvecreative.com>
2013-05-10 15:23:05 -0400

    Fix the API call, which leaaves it broken still.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -106,15 +106,16 @@ class UsersController < ApplicationController
             add_ids=""
             remove_ids=""
             
-            add_ids += params[:ecomm_newsletter] ? "1021, " : ""
-            add_ids += params[:ecomm_important] ? "1041, " : ""
-            add_ids += params[:ecomm_campaign] ? "1042, " : ""
-            remove_ids += params[:ecomm_newsletter] ? "" : "1021, "
-            remove_ids += params[:ecomm_important] ? "" : "1041, "
-            remove_ids += params[:ecomm_campaign] ? "" : "1042, "
+            add_ids += params[:ecomm_newsletter] ? "1021," : ""
+            add_ids += params[:ecomm_important] ? "1041," : ""
+            add_ids += params[:ecomm_campaign] ? "1042," : ""
+            remove_ids += params[:ecomm_newsletter] ? "" : "1021,"
+            remove_ids += params[:ecomm_important] ? "" : "1041,"
+            remove_ids += params[:ecomm_campaign] ? "" : "1042,"
+            add_ids = "justatestforErrorResponse"
             puts "Add ids: " + add_ids
             puts "Remove ids: " + remove_ids
-            c.update(@cont.cv__Convio_ID__c.to_i, add_interest_ids=add_ids, remove_interest_ids=remove_ids, blah=123)
+            c.update(@cont.cv__Convio_ID__c.to_i, add_interest_ids=add_ids, remove_interest_ids=remove_ids)
           end
           format.html { redirect_to "/mymoh/account", notice: 'User was successfully updated.' }
           format.json { head :ok }
