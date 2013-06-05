05ea847a81af336bc1c19d1b91a310996107274f
Jesse Briggs <j@pc>
2013-06-05 18:27:18 -0400

    Add status_check

--- config/routes.rb
+++ config/routes.rb
@@ -282,6 +282,7 @@ Mohhaiti::Application.routes.draw do
   match "/console" => "page#console"
   
   get "mymoh/account"
+  get "mymoh/status_check"
   get "mymoh/home"
   get "mymoh/giving"
   get "mymoh/giving/edit" => "mymoh#givingedit"
