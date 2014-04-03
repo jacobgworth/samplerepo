d903b8aa4ce53edcb8d14a170cc5e7e861654f94
David Dewhirst <dsdewhir@gmail.com>
2014-04-03 12:22:05 -0400

    .

--- app/controllers/mymoh_controller.rb
+++ app/controllers/mymoh_controller.rb
@@ -180,7 +180,7 @@ class MymohController < ApplicationController
   def sponsorships
     dbdc_client.materialize("cv__Recurring_Gift__c")
 	#@gifts = Cv__Recurring_Gift__c.find_all_by_cv__Contact__c(current_user.convio_id)
-	@gifts = Cv__Recurring_Gift__c.query("cv__Contact__c = '" + current_user.convio_id + "' AND Child_Status__c = 'Sponsored'")
+	@gifts = Cv__Recurring_Gift__c.query("cv__Contact__c = '" + current_user.convio_id + "' AND Child_Status__c = 'Sponsored' ")
     @account = Contact.find_by_Id(current_user.convio_id)
     #@old_sponsorships = Child_Sponsorship__c.query("Sponsor__c = '" + current_user.convio_id + "' AND Status__c = 'Open'")
 	@sponsorships = @gifts # + @old_sponsorships
