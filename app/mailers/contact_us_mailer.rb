1bce89762e52d1b36661069006a758c1c36f58f9
Jacob Worth <Jacob@Jacobs-MacBook-Pro.local>
2015-05-10 14:52:46 -0500

    Take off mailer again

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -118,22 +118,6 @@ class ContactUsMailer < ActionMailer::Base
     
     mail(:to => @toemail, :subject => "Take a Medical Mission Trip", :from => @fromaddress)
   end
-
-  def choose_project(data)
-    #change this to medical@mohhaiti.org after testing
-    
-    @data = data
-    
-    @fullname = @data[:fname]
-    @phone = @data[:phone]
-    @fromaddress = @data[:fromaddress]
-    @comments = @data[:comments]
-    @license = @data[:license]
-    @toemail = "medical@mohhaiti.org"
-    #@toemail = "david@threetwelvecreative.com" #testing
-    
-    mail(:to => @toemail, :subject => "Take a Medical Mission Trip", :from => @fromaddress)
-  end
   
   def take_a_trip(data)
     #change this to missiontrips@mohhaiti.org after testing
