be518d94c9a23f6006861d67b601632b50270d4d
Jacob Worth <Jacob@Jacobs-MacBook-Pro.local>
2015-05-16 09:25:34 -0500

    guess i didn't save this one

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -980,9 +980,3 @@ class PageController < ApplicationController
   
 end
 
-class Project < ActiveRecord::Base
-  has_one :project_type
-
-  # Examples of validations.
-  validates_presence_of :team_leader, :team_name, :project_type
-end
